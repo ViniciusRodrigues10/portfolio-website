2385d510e16050c27a908b381754364ddfde19c7
Vinicius Rodrigues <viniciusgcr1710@gmail.com>
2024-03-15 22:40:38 -0300

    feat: adding id to sessions

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -1,6 +1,5 @@
 "use client"
 import React, { useTransition, useState } from "react";
-import Image from "next/image";
 import TabButton from "./TabButton";
 
 const TAB_DATA = [
@@ -51,11 +50,10 @@ const AboutSection = () => {
     }
 
     return (
-        <section className="text-white">
+        <section id="about" className="text-white h-screen">
             <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-                <Image src="/images/about-image.png" width={500} height={500} />
                 <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
-                    <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+                    <h2 className="text-4xl font-bold text-white mb-4">About</h2>
                     <p className="text-base lg:text-lg">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid ipsa eaque ea nisi explicabo ullam, quod at alias? Laboriosam quod quaerat ut nobis quas eveniet ad provident asperiores, cum illo.</p>
                     <div className="flex flex-row justify-start mt-8">
                         <TabButton selecTab={() => handleTabChange("skills")} active={tab === "skills"}>
