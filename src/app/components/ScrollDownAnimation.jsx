48c9115ffcb6c420592d4e4b0f52b0a1d5e78e93
Vinicius Rodrigues <viniciusgcr1710@gmail.com>
2024-03-15 22:45:29 -0300

    feat: mouse scrolling animation

---
+++ src/app/components/ScrollDownAnimation.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import styles from './ScrollComponent.module.css';
+const ScrollDownAnimation = () => {
+  return (
+    <div className={styles.down}></div>
+  );
+};
+
+export default ScrollDownAnimation;
