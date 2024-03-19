5eab2cfc0e3a3f78eb6bf5a3823e65b1b8899d64
Vinicius Rodrigues <viniciusgcr1710@gmail.com>
2024-03-19 11:30:53 -0300

    feat: creating cube

---
+++ src/app/components/Cube3D.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import styles from '../../../styles/Cube3d.module.scss';
+import {
+    faAngular,
+    faCss3,
+    faGitAlt,faHtml5,
+    faJsSquare,
+    faReact,
+} from '@fortawesome/free-brands-svg-icons';
+
+
+const CubeComponent = () => {
+  return (
+
+    <div className={styles.cubespinner}>
+        <div className={styles.face1}>
+            <FontAwesomeIcon icon={faAngular} color="#DD0031" />
+        </div>
+        <div className={styles.face2}>
+            <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+        </div>
+        <div className={styles.face3}>
+            <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+        </div>
+        <div className={styles.face4}>
+            <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+        </div>
+        <div className={styles.face5}>
+            <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+        </div>
+        <div className={styles.face6}>
+            <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+        </div>
+    </div>
+
+  );
+};
+
+export default CubeComponent;
