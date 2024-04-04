943a7135056b6235e4a082e665801d7618c8f6d5
Vinicius Rodrigues <viniciusgcr1710@gmail.com>
2024-04-03 23:46:06 -0300

    refactor: using nodemailer to send emails

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -1,32 +1,150 @@
-import { NextResponse } from "next/server";
-import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
-// const fromEmail = process.env.FROM_EMAIL;
-// console.log(fromEmail);
+// import { NextResponse } from "next/server";
+// import { Resend } from "resend";
 
-export async function POST(req, res) {
-  // const { body } = await req.json();
-  // const { email, subject, message } = body;
-  // const { email, subject, message } = await req.json();
-  // console.log(email, subject, message);
+// const resend = new Resend(process.env.RESEND_API_KEY);
+// // const fromEmail = process.env.FROM_EMAIL;
+// // console.log(fromEmail);
+
+// export async function POST(req, res) {
+//   // const { body } = await req.json();
+//   // const { email, subject, message } = body;
+//   // const { email, subject, message } = await req.json();
+//   // console.log(email, subject, message);
+//   try {
+//     const data = await resend.emails.send({
+//       from: "viniciusgcr1710@gmail.com",
+//       to: ['vinicius.gonzaga@academico.ifpb.edu.br'],
+//       subject: "teste com email pessoal",
+//       react: (
+//         <>
+//           <h1>{"teste"}</h1>
+//           <p>Thank you for contacting us!</p>
+//           <p>New message submitte</p>
+//           <p>{"message"}</p>
+//         </>
+//       ),
+//     });
+
+//     return NextResponse.json(data);
+//   } catch (error) {
+//     return NextResponse.json({ error });
+//   }
+// }
+
+// import { NextResponse } from "next/server";
+
+// export async function GET(request) {
+//   const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+
+//   if(!res.ok) throw new Error("Problemas com o Fetch");
+
+//   const data = await res.json();
+
+//   return NextResponse.json({data});
+// }
+
+
+// ----------------------------------------------------------------------
+
+// import { transporter } from "../../../../config/nodemailer";
+
+// export async function POST(req, res) {
+//   const data = req.body;
+//   try {
+//     await transporter.sendMail({
+//       ...mailOptions, 
+//       subject: data.subject,
+//       text: "This is a test string",
+//       html: "<h1> Test Title</h1><p>Some body text</p>",
+//     });
+
+//     return NextResponse.json(data);
+//   }
+//   catch (error) {
+//     return NextResponse.json({ error });
+//   }
+// }
+
+
+
+// ----------------------------------------------------------------------
+
+
+
+// import { NextResponse } from 'next/server';
+// import nodemailer from 'nodemailer';
+
+// export async function POST(request) {
+//   const email = process.env.EMAIL;
+//   const pass = process.env.EMAIL_PASS;
+
+//   const transporter = nodemailer.createTransport({
+//     service: "gmail",
+//     auth: {
+//       user: email,
+//       pass: "r s b w c z x b bb b f bfs y k q",
+//     },
+//   });
+
+//   const mailOptions = {
+//     from:email,
+//     to: email,
+//     subject: "Send Email Tutorial",
+//     html:
+//     '<h3>Ola Vinicius</h3>'
+//   }
+
+//   try {
+//     await transporter.sendMail(mailOptions);
+
+//     return NextResponse.json({message: "Email enviado"}, {status: 200});
+//   } catch (error) {
+//     return NextResponse.json({ message: "Failed to send" }, {status: 500});
+//   } 
+// }
+
+
+// ----------------------------------------------------------------------
+import { NextResponse } from 'next/server';
+import nodemailer from 'nodemailer';
+
+export async function POST(request, response) {
+  const {name, clientEmail, subject, message} = await request.json();
+  console.log(name, clientEmail, subject, message)
+
+  const contactEmail = process.env.EMAIL;
+  const pass = process.env.EMAIL_PASS;
+
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: contactEmail,
+      pass: pass
+    }
+  });
+
+  const mailOptions = {
+    from: clientEmail,
+    to: contactEmail,
+    subject: subject,
+    text: message
+  };
+
+  const mailOptionsClient = {
+    from: contactEmail,
+    to: clientEmail,
+    subject: "Contato",
+    text: "TEste"
+  };
+
+
   try {
-    const data = await resend.emails.send({
-      from: "viniciusgcr1710@gmail.com",
-      to: ['vinicius.gonzaga@academico.ifpb.edu.br'],
-      subject: "teste com email pessoal",
-      react: (
-        <>
-          <h1>{"teste"}</h1>
-          <p>Thank you for contacting us!</p>
-          <p>New message submitte</p>
-          <p>{"message"}</p>
-        </>
-      ),
-    });
+    await transporter.sendMail(mailOptions);
+    await transporter.sendMail(mailOptionsClient);
 
-    return NextResponse.json(data);
+    return NextResponse.json({message: "Email enviado"}, {status: 200});
   } catch (error) {
-    return NextResponse.json({ error });
-  }
+    return NextResponse.json({ message: "Failed to send" }, {status: 500});
+  } 
 }
--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -4,32 +4,37 @@ import GithubIcon from "../../../public/github-icon.svg";
 import LinkedinIcon from "../../../public/linkedin-icon.svg";
 import Link from "next/link";
 import Image from "next/image";
+import styles from "../../../styles/SocialIcons.module.css";
 
 const EmailSection = () => {
+    const [formData, setFormData] = useState({
+        name: '',
+        clientEmail: '',
+        subject: '',
+        message: ''
+    });
+
     const [emailSubmitted, setEmailSubmitted] = useState(false);
 
-    const handleSubmite = async(e) => {
-        e.preventDefault();
-        const data = {
-            email: e.target.email.value,
-            subject: e.target.subject.value,
-            message: e.target.message.value,
-        };
-        const JSONdata = JSON.stringify(data);
-        const endpoint = "/api/send";
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value
+        });
+    };
 
-        const options = {
+    const handleSubmit = async (e) => {
+        e.preventDefault(); 
+        const response = await fetch('/api/send', {
             method: 'POST',
-
             headers: {
-                'Content-Type': 'application/json',
+                'content-type': 'application/json',
+                Accept: "aplication/json",
             },
-
-            body: JSONdata, 
-        };
-
-        const response = await fetch(endpoint, options);
-        const resData = await response.json();
+            body: JSON.stringify(formData)
+        })
+        console.log(await response.json())
 
         if (response.status === 200) {
             console.log('Message sent.');
@@ -39,59 +44,88 @@ const EmailSection = () => {
     
     return (
     <section id="contact" className='grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative'>
-        <div className='absolute ame="absolute w-80 h-80 z-0'></div>
+        <div className='absolute w-80 h-80 z-0'></div>
         <div className='z-10'>
-            <h5 className='text-xl font-bold text-white my-2'>Let's Connect</h5>
+            <h5 className='text-xl font-bold text-white my-2'>Entre em Contato</h5>
             <p className='text-[#ADB7BE] mb-4 max-w-md'>
                 {" "}   
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid cupiditate beatae nemo exercitationem facere veritatis laborum deleniti tempore quas nulla excepturi voluptatibus deserunt odio, minima aut eaque ab voluptatum velit!
+                Quer iniciar um projeto, discutir uma ideia ou simplesmente trocar algumas palavras? Estou aqui para ajudar! Você também pode me contratar para trabalhos freelance ou oportunidades de emprego. Entre em contato comigo e vamos transformar sua visão em realidade.
             </p>
             <div className='social flex flex-row gap-2'>
-                <Link href="guthub.com">
-                    <Image src={GithubIcon} alt="Github Icon" />
+                <Link href="https://github.com/ViniciusRodrigues10" target="_blank">
+                    <div className={styles.iconWrapper}>
+                        <Image src={GithubIcon} alt="Github Icon" className={styles.icon} />
+                    </div>
                 </Link>
-                <Link href="linkedin.com">
-                    <Image src={LinkedinIcon} alt="Github Icon" />
+                <Link href="https://www.linkedin.com/in/viniciusgonzagacavalcante" target="_blank">
+                    <div className={styles.iconWrapper}>
+                        <Image src={LinkedinIcon} alt="Linkedin Icon" className={styles.icon} />
+                    </div>
                 </Link>
             </div>
         </div>
         <div>
-            <form action="https://formsubmit.co/007d0733b41c1d57bbc3e636b88391ae" method="POST" className='flex flex-col'>
+            <form onSubmit={handleSubmit} className='flex flex-col'>
                 <div className='mb-6'>
-                    <label for="email" className="block mb-2 text-sm font-medium text-white">
+                    <label htmlFor="name" className="block mb-2 text-sm font-medium text-white">
+                        Name
+                    </label>
+                    <input 
+                        name="name"
+                        type="text"
+                        id="name"
+                        required 
+                        value={formData.name}
+                        onChange={handleChange}
+                        className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5" 
+                        placeholder='Your name'
+                    />
+                </div>
+                <div className='mb-6'>
+                    <label htmlFor="clientEmail" className="block mb-2 text-sm font-medium text-white">
                         Your email
                     </label>
                     <input 
-                        name="email"
+                        name="clientEmail"
                         type="email"
-                        id="email"
+                        id="clientEmail"
                         required 
+                        value={formData.clientEmail}
+                        onChange={handleChange}
                         className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5" 
                         placeholder='jacob@google.com'
                     />
                 </div>
-                {/* <div className="mb-6">
-                    <label for="subject" className="block mb-2 text-sm font-medium text-white">
+                <div className="mb-6">
+                    <label
+                        htmlFor="subject" 
+                        className="block mb-2 text-sm font-medium text-white"
+                    >
                         Subject
                     </label>
                     <input
-                        name="email"
+                        name="subject"
                         type="text"
                         id="subject"
+                        value={formData.subject}
+                        onChange={handleChange}
                         className="bg-gray-[#18191E] border border-[#33353F] bg-[#18191E] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg  block w-full p-2.5"
                         placeholder="Just saying hi"
                     /> 
-                </div> */}
+                </div>
                 <div className='mb-6'>
                     <label
-                        for="message"
-                        className="block mb-2 text-sm font-medium text-white">
-                            Message
+                        htmlFor="message"
+                        className="block mb-2 text-sm font-medium text-white"
+                    >
+                        Message
                     </label>
                     <input 
                         type="text"
                         name="message"
                         id="message"
+                        value={formData.message}
+                        onChange={handleChange}
                         className="bg-gray-[#18191E] border border-[#33353F] bg-[#18191E] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg  block w-full p-2.5"
                         placeholder="Let's talk about..." 
                     />
@@ -102,13 +136,13 @@ const EmailSection = () => {
                 >
                     Send Message    
                 </button>
-                {/* {
+                {
                     emailSubmitted && (
                         <p className="text-green-500 text-sm mt-2">
                             Email sent sucessfully!
                         </p>
                     )
-                } */}
+                }
             </form>
         </div>    
     </section>
