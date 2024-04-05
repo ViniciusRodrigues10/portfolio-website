9029b9a36bec4aa7f0f9ff6afea053da4e087cfb
Vinicius Rodrigues <viniciusgcr1710@gmail.com>
2024-04-05 16:10:13 -0300

    feat: creating loading spinner effect

---
+++ src/app/components/LoadingSpinner.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from '../../../styles/LoadingSpinner.module.css';
+
+const LoadingSpinner = () => {
+    return (
+        <div className={styles.spinnercontainer}>
+            <div className={styles.spinner}></div>
+        </div>
+    );
+}
+
+export default LoadingSpinner;
