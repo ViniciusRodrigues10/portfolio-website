ec4b3404e2c39cb806bcc69b3b3b6533960c8876
Vinicius Rodrigues <viniciusgcr1710@gmail.com>
2024-05-09 13:00:17 -0300

    fix: fixing footer responsiveness and adding year automatically

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
+const currentYear = new Date().getFullYear();
+
 const Footer = () => {
   return (
-    <footer className='footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white'>
-        <div className='container p-12 flex justify-between'>
+    <footer className='footer border z-10 border-t-[#363535] border-b-transparent border-l-transparent border-r-transparent text-white'>
+        <div className='container p-12 justify-between mx-auto md:flex'>
           <span>Vinícius Rodrigues</span>
-          <p className='text-slate-600'>Todos os direitos reservados.</p>
+          <p className='text-slate-600 mt-4 md:mt-0'>{currentYear} Todos os direitos reservados.</p>
         </div>
     </footer>
   )
